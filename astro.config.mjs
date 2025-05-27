06d52a33f4d079a1a605e72a3f4bf6f9910286a1
rwaddinsall <r.addinsall@gmail,com>
2025-05-27 11:27:57 +1000

    Initial commit: Setup project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [react(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
