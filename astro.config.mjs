1a71535fdba48a15dc77c72716692633e4577840
rwaddinsall <r.addinsall@gmail,com>
2025-06-07 22:42:33 +1000

    info page grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [react(), sitemap()],
-
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-// 
 module.exports = {
   content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-     
-    },
+    extend: {},
   },
   plugins: [require('daisyui')],
-  
 };
